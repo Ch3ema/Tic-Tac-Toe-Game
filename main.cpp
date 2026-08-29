e12fb3dfa0e3d11404ee76c58fd4489f55926dca
Ch3ema <aliazharcheemaa@gmail.com>
2026-08-29 11:58:10 -0400

    Game is now functional. We have alternating turns, and the board is printing the correct inputs. (Winning condion and prohibited actions next)

--- main.cpp
+++ main.cpp
@@ -2,25 +2,66 @@ using namespace std;
 #include <iostream>
 #include <string>
 
+
+char board [3][3];
+int chosenRow;
+int chosenCol;
+int turnCounter = 0;
+
+
+void printBoard () { //Function to print board
+    for(int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << board[i][j];
+        }
+        cout << "\n";
+    }
+}
+
+
+void newPlayerTurn() {
+
+int playerNumber = (turnCounter % 2 == 0) ? 1 : 2;
+char actionChar = (turnCounter % 2 == 0) ? 'X' : 'O';
+
+    cout << "Please enter the row and col of your action. \n";
+    cout << "Action on Player" << playerNumber << ":  \n";
+    cin >> chosenRow >> chosenCol;
+
+    board[chosenRow - 1][chosenCol - 1] = actionChar;
+    turnCounter++;
+}
+
+
 int main() {
-
-    char board [3][3];
-
+    
     //fills the matrix with '*' as place holder
     for (int i= 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             board [i][j] = '*';
         }
     }
+    //call the function to place the first board
+    printBoard();
+        newPlayerTurn(); // Turn 1
 
-    //prints the matrix
-    for (int i= 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-           cout << board [i][j] << " ";
-        }
-        cout << "\n";
-    }
 
+    //call the function to place the first X
+    printBoard();
+        newPlayerTurn(); // Turn 2
+
+   
+    printBoard();
+        newPlayerTurn(); // Turn 3
+
+    printBoard();
+        newPlayerTurn(); // Turn 4
+
+    printBoard();
+        newPlayerTurn(); // Turn 5
+
+    printBoard();
+        newPlayerTurn(); // Turn 6
 
     
     return 0;
