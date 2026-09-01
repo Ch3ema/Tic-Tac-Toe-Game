c8df0a3f56d7a2f9b88b3e745c7d6edbb3268639
Ch3ema <aliazharcheemaa@gmail.com>
2026-09-01 18:07:11 -0400

    Prohibited action conditions added. Game is functionally now complete.

--- main.cpp
+++ main.cpp
@@ -30,35 +30,43 @@ char actionChar = (turnCounter % 2 == 0) ? 'X' : 'O';
     cout << "Action on Player " << playerNumber << ":  \n";
     cin >> chosenRow >> chosenCol;
 
-
+    //prohited action conditions
+    while (chosenRow < 1 || chosenRow > 3 || chosenCol< 1 || chosenCol > 3 
+        || board[chosenRow - 1][chosenCol -1 ] != '*' || cin.fail()) 
+        {
+            cout << "Invalid input, please try again. \n";
+            cin.clear(); 
+            cin.ignore(1000, '\n');
+            cin >> chosenRow >> chosenCol;
+        }
+    
     board[chosenRow - 1][chosenCol - 1] = actionChar;
     turnCounter++;
 }
 
 //win conditions
 bool checkWin() {
+    //Row win 
     for (int i = 0; i < 3; i++){
         if ((board[i][0] == board[i][1] && board[i][0] == board[i][2]) && board[i][0] != '*') {
         return true;
         }
-    
+    //Col win
     for (int i = 0; i < 3; i++){
         if ((board[0][i] == board[1][i] && board[0][i] == board[2][i]) && board[0][i] != '*') {
         return true;
         }
-    }   
+    }
+    //primary diagonal win   
     if (board [0][0] == board[1][1] && board [0][0] == board [2][2] && board [0][0] != '*') {
         return true;
     }
+    //secondary diagonal win
     if (board [2][0] == board[1][1] && board [2][0] == board [0][2] && board [2][0] != '*') {
         return true;
     }
 } return false; 
 }
-
-
-
-
 
 int main() {
     
@@ -75,12 +83,10 @@ int main() {
 
         printBoard();
             newPlayerTurn();
-
                if (checkWin()) {
                 cout << "Player " << playerNumber << " wins! \n";
                 printBoard();
-                break;
-            
+                break;       
     }
 }
     return 0;
